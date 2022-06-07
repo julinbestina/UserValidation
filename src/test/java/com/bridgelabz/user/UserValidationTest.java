7d9b985dd5ca68ddaa421091f9db71c58b556850
julinbestina <julin.bestina@yahoo.com>
2022-06-07 11:49:07 +0530

    Added functionality to validate LastName

--- src/test/java/com/bridgelabz/user/UserValidationTest.java
+++ src/test/java/com/bridgelabz/user/UserValidationTest.java
@@ -1,5 +1,21 @@
 package com.bridgelabz.user;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class UserValidationTest {
+    UserValidation user = new UserValidation();
+
+    @Test
+    public void givenFirstName_WhenValid_ShouldReturnTrue() {
+        boolean isValid = user.validateFirstName("Julin");
+        Assert.assertTrue(isValid);
+    }
+
+    @Test
+    public void givenLastName_WhenValid_ShouldReturnTure() {
+        boolean isValid = user.validateLastName("Bestina");
+        Assert.assertTrue(isValid);
+    }
 
 }
